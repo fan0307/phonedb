dfde3afed148b6578a91dc7e59bec6da181ee98e
Fan Xu <fxu@futuredial.com>
2023-10-05 12:10:08 -0700

    phone CRUD

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/user');
+const Phone = require('./models/phone');
 
 // express app
 const app = express();
@@ -27,7 +28,7 @@ app.use(express.urlencoded({ extended: true })); //this will helps to get submit
 
 // home routes
 app.get('/', (req, res) => {
-  res.redirect('/users'); //this will redirect page to /users
+  res.redirect('/phones'); //this will redirect page to /users
 });
 
 //users i.e index route
@@ -36,6 +37,17 @@ app.get('/users',(req,res)=>{
    User.find().sort({createdAt:-1})//it will find all data and show it in descending order
     .then(result => { 
       res.render('index', { users: result ,title: 'Home' }); //it will then render index page along with users
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
+//phones i.e index route
+app.get('/phones',(req,res)=>{
+  console.log("req made on"+req.url);
+   Phone.find().sort({createdAt:-1})//it will find all data and show it in descending order
+    .then(result => { 
+      res.render('phone', { phones: result ,title: 'Phone-Home' }); //it will then render index page along with users
     })
     .catch(err => {
       console.log(err);
@@ -53,6 +65,11 @@ app.get('/user/create',(req,res)=>{
   console.log("GET req made on"+req.url);
   res.render('adduser',{title:'Add-User'});
 })
+//route for phone create
+app.get('/phone/create',(req,res)=>{
+  console.log("GET req made on"+req.url);
+  res.render('addphone',{title:'Add-Phone'});
+})
 
 //route for users/withvar
 app.get('/users/:id', (req, res) => {
@@ -60,6 +77,17 @@ app.get('/users/:id', (req, res) => {
   User.findById(id)
     .then(result => {
       res.render('details', { user: result, action:'edit',title: 'User Details' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+//route for phones/withvar
+app.get('/phones/:id', (req, res) => {
+  const id = req.params.id;
+  Phone.findById(id)
+    .then(result => {
+      res.render('details-phone', { phone: result, action:'edit',title: 'Phone Details' });
     })
     .catch(err => {
       console.log(err);
@@ -73,6 +101,18 @@ app.get('/edit/:name/:action',(req,res)=>{
   User.findOne({name:name})
     .then(result => {
       res.render('edit', { user: result ,title: 'Edit-User' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
+//route for PHONE edit/name/action variable that will display current value to input field
+app.get('/edit-phone/:esn/:action',(req,res)=>{
+  const esn = req.params.esn;
+  console.log("req made on"+req.url);
+  Phone.findOne({esn:esn}) 
+    .then(result => {
+      res.render('edit-phone', { phone: result ,title: 'Edit-Phone' });
     })
     .catch(err => {
       console.log(err);
@@ -98,7 +138,25 @@ app.get('/edit/:name/:action',(req,res)=>{
     });
 
 })
+//submitting phone data routes
+app.post('/phone/create',(req,res)=>{
+  console.log("POST req made on"+req.url);
+  console.log("phone data submitted to server");
 
+
+  /*Note: when you are passing form obj directly to collection using model you
+          have to give same name in form of that data that is to be passed in database 
+          and that name is declared inside schema*/
+  const phone = new Phone(req.body); //passing object of form data directly to collection
+  phone.save() //then saving this to database and this return promise
+    .then(result => {
+      res.redirect('/phones');//is success save this will redirect to home page
+    })
+    .catch(err => { //if data not saved error showed
+      console.log(err);
+    });
+
+})
 //route for updating users data
 app.post('/edit/:id',(req,res)=>{
   console.log("POST req made on"+req.url);
@@ -113,7 +171,20 @@ app.post('/edit/:id',(req,res)=>{
     });
 
 })
+//route for updating phones data
+app.post('/edit-phone/:id',(req,res)=>{
+  console.log("POST req made on"+req.url);
+  Phone.updateOne({_id:req.params.id},req.body) //then updating that user whose id is get from url 
+                                               //first passing id which user is to be updated than passing update info
+    .then(result => {
+      res.redirect('/phones');//is success save this will redirect to home page
+      console.log("Phones profile Updated");
+    })
+    .catch(err => { //if data not saved error showed
+      console.log(err);
+    });
 
+})
 
 //routes for deleting users by getting users name from url then finding that  users then doing delete
 app.post('/users/:name',(req,res)=>{ //form action of details.ejs pass name of user that later is assume as name
@@ -127,7 +198,18 @@ app.post('/users/:name',(req,res)=>{ //form action of details.ejs pass name of u
     console.log(err);
   });
 })
-
+//routes for deleting phones by getting phones esn from url then finding that phones then doing delete
+app.post('/phones/:esn',(req,res)=>{ //form action of details.ejs pass name of user that later is assume as name
+  const esn = req.params.esn;
+  console.log(esn);
+  Phone.deleteOne({esn:esn})
+  .then(result => {
+    res.redirect('/phones');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+})
 //404 errors routes
 //this will auto run incase no routes
 //Note: must put this route at last route list
